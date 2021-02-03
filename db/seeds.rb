a3fd4672244765ae5ecd06ee465d69f66d030454
sw <tansw98@gmail.com>
2021-02-03 17:38:38 +0800

    add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,56 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Profile.create([
+  {
+    name: 'Tan See Wei',
+    address: '5, Jalan Perdana 5',
+    phone: '0123456789',
+    email: 'example@abc.com',
+    linkedin: 'linkedin.com/in/example',
+    github: 'github.com/swtan98',
+  }
+])
+
+Experience.create([
+  {
+    title: 'Frontend Web Dev',
+    company: 'HLA Integrated',
+    date: '05/2020 - 12/2020',
+  },
+  {
+    title: 'Frontend Web Dev',
+    company: 'Inventrix Solutions',
+    date: '03/2020 - 04/2020',
+  },
+])
+
+Education.create([
+  {
+    title: 'BSc in Computer Science (Data Analytics)',
+    school: 'Asia Pacific University',
+    date: '03/2020',
+  },
+  {
+    title: 'Diploma in ICT',
+    school: 'Asia Pacific University',
+    date: '05/2018',
+  },
+])
+
+Skills.create([
+  {
+    title: 'React.js',
+    description: 'Built and integrated forms and tables with backend data',
+  },
+  {
+    title: 'Python',
+    description: 'Python for NLTK and machine learning',
+  },
+])
+  {
+    title: 'Apollo GraphQL',
+    description: 'Integrated forms and tables in frontend making use of GraphQL queries and mutations',
+  },
+])
